8b164b0eb459cbbf01cbabc2ed02707f7de57382
Aryodjat1 <aryodjati5@gmail.com>
2022-10-24 10:52:32 +0700

    Add typeData&Variable.kt

---
+++ Kotlin Fundamentals/kotlin-fundamentals/src/main/kotlin/typeData&Variable.kt
@@ -0,0 +1,31 @@
+//     FORMULA :
+//     var identifier: Type = initialization
+
+fun main() {
+    var company: String = "Dicoding"
+
+
+    // VAR is changeable
+    var company: String = "Dicoding"
+    company = "Dicoding Academy"
+
+//    VAL is unchangeable
+    val company: String = "Dicoding"
+    company = "Dicoding Academy" //Val cannot be reassigned
+
+//    Operator + On Print
+    val firstWord = "Dicoding "
+    val lastWord = "Academy"
+    print(firstWord + lastWord)
+    /*
+   output: Dicoding Academy
+    */
+
+//    Merge 2 Variable
+    val valueA: Int = 10
+    val valueB = 20
+    print(valueA + valueB)
+    /*
+       output: 30
+    */
+}
