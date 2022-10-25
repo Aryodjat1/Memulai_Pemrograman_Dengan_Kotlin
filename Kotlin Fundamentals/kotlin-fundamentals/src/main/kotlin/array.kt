09eaf310bb3930606c61c9ff1008006a7086d17f
Aryodjat1 <aryodjati5@gmail.com>
2022-10-25 10:07:33 +0700

    add array course

---
+++ Kotlin Fundamentals/kotlin-fundamentals/src/main/kotlin/array.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val array = arrayOf(1, 3, 5, 7)
+    val mixArray = arrayOf(1, 3, 5, 7 , "Dicoding" , true)
+
+    val intArray = Array(4, { i -> i * i }) // Menggunakan Library Function
+}
+
+/*  KONVERSI ARRAY
+    intArrayOf() : IntArray
+
+    booleanArrayOf() : BooleanArray
+
+    charArrayOf() : CharArray
+
+    longArrayOf() : LongArray
+
+    shortArrayOf() : ShortArray
+
+    byteArrayOf() : ByteArray
+
+*/
