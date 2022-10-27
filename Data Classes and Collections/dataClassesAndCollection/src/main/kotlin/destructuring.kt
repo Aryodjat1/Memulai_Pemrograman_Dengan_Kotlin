f4fdd2b052a2afd590c66f74069ca1bd667ef348
Aryodjat1 <aryodjati5@gmail.com>
2022-10-27 21:52:01 +0700

    add destructuring course

---
+++ Data Classes and Collections/dataClassesAndCollection/src/main/kotlin/destructuring.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val dataUser = DataUser("nrohmen", 17)
+    val (name, age) = dataUser
+
+    println("My name is $name, I am $age years old")
+
+//    Using ComponenN()
+    val dataUser1 = DataUser("nrohmen", 17)
+    val (name1, age1) = dataUser1
+
+    println("My name is $name1, I am $age1 years old")
+}
