7ce15d1e8dc1dcbbce01893d4c89076441c2db4e
Aryodjat1 <aryodjati5@gmail.com>
2022-10-28 11:14:39 +0700

    add readme Control flow

---
+++ Kotlin Fundamentals/kotlin-fundamentals/src/main/kotlin/stringTemplate.kt
@@ -0,0 +1,11 @@
+//  String Template
+//  "First character of $text is $firstChar"
+
+fun main() {
+    val name = "Kotlin"
+    print("My name is " + name)
+}
+/*
+   output : My name is Kotlin
+*/
+
