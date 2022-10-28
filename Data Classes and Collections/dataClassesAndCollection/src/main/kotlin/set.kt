b22a7d19a4e3cc36602e13bf80e968d767ab347a
Aryodjat1 <aryodjati5@gmail.com>
2022-10-28 10:06:27 +0700

    add set course

---
+++ Data Classes and Collections/dataClassesAndCollection/src/main/kotlin/set.kt
@@ -0,0 +1,22 @@
+// Set is Collection with Unic value
+
+fun main() {
+    val integerSet = setOf(1, 2, 4, 2, 1, 5)
+    val setA = setOf(1, 2, 4, 2, 1, 5)
+    val setB = setOf(1, 2, 4, 5)
+    val setC = setOf(1, 5, 7)
+    val union = setA.union(setC)
+    val intersect = setA.intersect(setC)
+
+    println(integerSet)
+    print(5 in setA)
+    println(union)
+    println(intersect)
+
+
+//    Mutable Set
+    val mutableSet = mutableSetOf(1, 2, 4, 2, 1, 5)
+    //mutableSet[2] = 6 // tidak bisa mengubah set immutable
+    mutableSet.add(6) // menambah item di akhir set
+    mutableSet.remove(1) //menghapus item yang memiliki nilai 1
+}
